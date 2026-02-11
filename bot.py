5b2665ceeff76d7aa807d1a94f9d64ae6d742710
imrrobat <imrrobat@gmail.com>
2026-02-11 14:09:47 +0330

    input add

--- bot.py
+++ bot.py
@@ -14,14 +14,43 @@ async def help_handler(pm: Message):
     await pm.answer("به زودی در این مکان، هلپ نصب میشود")
 
 
-async def add_handler(pm: Message, command: CommandObject):
-    task = command.args
+async def task_handler(message: Message):
+    # جدا کردن خطوط
+    lines = message.text.strip().splitlines()
 
-    if not task:
-        await pm.answer("اسم یه تسک رو وارد کن")
+    if len(lines) != 3:
+        await message.answer(
+            "لطفا پیام رو درست بفرستید:\nتیتر\n#دسته‌بندی\nعدد اولویت (1 تا 3)"
+        )
         return
 
-    await pm.answer(f"پس تو میخوای کار {task} رو انجام بدی")
+    title = lines[0].strip()
+    category_line = lines[1].strip()
+    priority_num = lines[2].strip()
+
+    # بررسی هشتگ
+    hashtags = [word for word in category_line.split() if word.startswith("#")]
+
+    if len(hashtags) != 1:
+        await message.answer(
+            "پیام شما باید دقیقا یک هشتگ داشته باشد. مثال:\n#سوپرمارکت"
+        )
+        return
+
+    category = hashtags[0][1:]  # حذف # از اول
+
+    # تبدیل اولویت عددی به متن
+    priority_map = {"1": "معمولی", "2": "مهم", "3": "فوری"}
+
+    priority_text = priority_map.get(priority_num)
+    if not priority_text:
+        await message.answer("عدد اولویت باید 1، 2 یا 3 باشد")
+        return
+
+    # پاسخ به کاربر
+    await message.answer(
+        f"کار شما با دسته‌بندی {category} و اولویت {priority_text} دریافت شد ✅"
+    )
 
 
 async def main():
@@ -30,7 +59,6 @@ async def main():
 
     dp.message.register(start_handler, CommandStart())
     dp.message.register(help_handler, Command("help"))
-    dp.message.register(add_handler, Command(commands=["add"]))
 
     await dp.start_polling(bot)
 
